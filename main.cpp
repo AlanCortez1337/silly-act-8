578b7f9738f5b6f0c82bb30d748051534616dc5d
Alan Cortez <acortez1337@csu.fullerton.edu>
2024-02-22 16:07:31 -0800

    add basic test for main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 
 int main() {
 
-    DLL<int> linkedList{};
+    DLL<int> TuffysTrain {};
 
-    linkedList.push_back(4);
+    TuffysTrain.push_front(24);
+    TuffysTrain.push_front(1);
+    TuffysTrain.push_front(7);
+    TuffysTrain.push_back(32);
+    TuffysTrain.push_back(400);
+    TuffysTrain.push_back(12);
+    TuffysTrain.insert(3, 2);
+
+    TuffysTrain.display();
+
+    TuffysTrain.pop_front();
+    TuffysTrain.pop_back();
+    TuffysTrain.deleteValue(400);
+
+    TuffysTrain.display();
+
+    TuffysTrain.reverse();
+
+    TuffysTrain.display();
 
     return 0;
 }
