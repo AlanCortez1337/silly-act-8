11e30e24c6efc37c8964e3c6e6b85c5fcda22985
Alan Cortez <78040578+AlanCortez1337@users.noreply.github.com>
2024-04-11 19:34:04 +0000

    add code to create a tree and delete nodes

--- main.cpp
+++ main.cpp
@@ -1,30 +1,24 @@
 #include "./dll/dll.hpp"
 #include "./stack/stack.hpp"
+#include "./bst/bst.hpp"
 
 int main() {
 
+    BST<int> mytree;
+
+    // Creating Tree
+    mytree.insert(8);
+    mytree.insert(14);
+    mytree.insert(3);
+    mytree.insert(27);
+    mytree.insert(11);
+
+    mytree.inorderPrint();
+
+    // Deleting Node
+    mytree.remove(14);
+    mytree.inorderPrint();
     
-    // DLL<int> TuffysTrain {};
-
-    // TuffysTrain.push_front(24);
-    // TuffysTrain.push_front(1);
-    // TuffysTrain.push_front(7);
-    // TuffysTrain.push_back(32);
-    // TuffysTrain.push_back(400);
-    // TuffysTrain.push_back(12);
-    // TuffysTrain.insert(3, 2);
-
-    // TuffysTrain.display();
-
-    // TuffysTrain.pop_front();
-    // TuffysTrain.pop_back();
-    // TuffysTrain.deleteValue(400);
-
-    // TuffysTrain.display();
-
-    // TuffysTrain.reverse();
-
-    // TuffysTrain.display();
 
     return 0;
 }
